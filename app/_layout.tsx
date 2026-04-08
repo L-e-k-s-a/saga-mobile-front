eda3bc9bda8cb7bea25d1cbbc3d8dc2ae3db5de0
Alexey <alexeyp542@gmail.com>
2026-04-08 22:13:22 +0300

    feat: change show screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,22 @@ import { Stack } from 'expo-router';
 import { useAuthStore } from '@/shared/store';
 import { ApolloProvider } from '@apollo/client/react';
 import { client } from '@/shared/client/client';
+
 export default function RootLayout() {
   const { isAuth } = useAuthStore();
 
   return (
     <ApolloProvider client={client}>
       <Stack screenOptions={{ headerShown: false }}>
-        {!isAuth ? (
+
+        <Stack.Protected guard={!isAuth}>
           <Stack.Screen name="(auth)" />
-        ) : (
+        </Stack.Protected>
+
+        <Stack.Protected guard={isAuth}>
           <Stack.Screen name="(tabs)" />
-        )}
+        </Stack.Protected>
+
       </Stack>
     </ApolloProvider>
   );
