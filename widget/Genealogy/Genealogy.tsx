68597c24f611507882626572f672a4aed0176669
Alexey <alexeyp542@gmail.com>
2026-04-15 20:02:31 +0300

    feat: added screen

---
+++ widget/Genealogy/Genealogy.tsx
@@ -0,0 +1,14 @@
+import { AlignContainer } from "@/shared/layouts/AlignContainer/AlignContainer"
+import { BackgroundContainer } from "@/shared/layouts/BackgroundContainer/BackgroundContainer"
+import { Typography } from "@/shared/ui/Typography/Typography"
+
+
+export const GenealogyWidget = () => {
+        return (
+                    <BackgroundContainer>
+                        <AlignContainer>
+                            <Typography>W</Typography>
+                        </AlignContainer>
+                    </BackgroundContainer>
+        )
+}
