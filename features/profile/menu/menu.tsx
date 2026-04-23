853212ea3e8929e20c2aae809f16ab464fe77533
Alexey Puchkov <alexeyp542@mail.ru>
2026-04-23 16:30:41 +0300

    feat: added logout

--- features/profile/menu/menu.tsx
+++ features/profile/menu/menu.tsx
@@ -1,20 +1,32 @@
-import { HorLayout } from "@/shared/layouts/HorLayout/HorLayout"
-import { VerLayout } from "@/shared/layouts/VerLayout/VerLayout"
-import { useAuthStore } from "@/shared/store"
-import { Button } from "@/shared/ui/buttons/button/Button"
-import { Ionicons } from "@expo/vector-icons"
-
+import { HorLayout } from '@/shared/layouts/HorLayout/HorLayout';
+import { VerLayout } from '@/shared/layouts/VerLayout/VerLayout';
+import { RoutesForAuth } from '@/shared/routes/routes';
+import { useAuthStore } from '@/shared/store';
+import { Button } from '@/shared/ui/buttons/button/Button';
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
 
 export const Menu = () => {
+	const { logout } = useAuthStore();
 
-    const { logout } = useAuthStore()
+	const handleLogout = async () => {
+		try {
+			await logout();
+			router.replace(RoutesForAuth.SIGN_IN);
+		} catch (error) {
+			console.error('Ошибка выхода:', error);
+		}
+	};
 
-    return (
-        <VerLayout>
-            <HorLayout>
-                <Ionicons />
-                <Button text="Выйти" onPress={logout}/>
-            </HorLayout>
-        </VerLayout>
-    )
-}
+	return (
+		<VerLayout>
+			<HorLayout>
+				<Ionicons />
+				<Button
+					text='Выйти'
+					onPress={handleLogout}
+				/>
+			</HorLayout>
+		</VerLayout>
+	);
+};
