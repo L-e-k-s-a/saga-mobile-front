36c4a37fc1761059a0889d9d4d6bc70aafcc6989
Alexey Puchkov <alexeyp542@mail.ru>
2026-04-27 16:39:51 +0300

    feat: added picker photo

--- features/profile/profile-image/profile-image.tsx
+++ features/profile/profile-image/profile-image.tsx
@@ -1,23 +1,39 @@
-import { COLORS } from "@/shared/constants/colors";
-import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet, TouchableOpacity, View } from "react-native"
+import { COLORS } from '@/shared/constants/colors';
+import { useImagePicker } from '@/shared/hooks/use-picker-image';
+import { Spinner } from '@/shared/ui/spinner/spinner';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 export const ProfileImage = () => {
+	const {image, pickImage, isLoading} = useImagePicker()
+
 	return (
-		<TouchableOpacity style={stileProfileImage.image}>
-                <Ionicons name="add" size={48} color={COLORS.white}/>
+		<TouchableOpacity
+			style={styleProfileImage.image}
+			onPress={() => pickImage([1,1])}
+            disabled={isLoading}
+            >
+			{image ? (
+				<Image style={styleProfileImage.image} source={{ uri: image }} />
+			) : (
+				<Ionicons
+					name='add'
+					size={48}
+					color={COLORS.white}
+				/>
+			)}
 		</TouchableOpacity>
 	);
 };
 
-
-const stileProfileImage = StyleSheet.create({
-    image: {
-        width: 100,
-        height: 100,
-        backgroundColor: COLORS.secondary,
-        borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+const styleProfileImage = StyleSheet.create({
+	image: {
+		width: 100,
+		height: 100,
+		backgroundColor: COLORS.secondary,
+		borderRadius: 50,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
