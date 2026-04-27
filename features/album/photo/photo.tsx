b7d2a2062500d9bd8e6dfe40b790e8ec1d35ebac
Alexey Puchkov <alexeyp542@mail.ru>
2026-04-27 16:50:03 +0300

    feat: added photo in album

--- features/album/photo/photo.tsx
+++ features/album/photo/photo.tsx
@@ -1,23 +1,28 @@
 import { COLORS } from '@/shared/constants/colors';
-import { Card } from '@/shared/ui/card/card';
+import { useImagePicker } from '@/shared/hooks/use-picker-image';
 import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, TouchableOpacity } from 'react-native';
-
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 type PhotoProps = {
-    photo: string
-}
+	photo: string;
+};
 
-export const Photo = ({photo}: PhotoProps) => {
+export const Photo = ({ photo }: PhotoProps) => {
+	const { image, pickImage, isLoading } = useImagePicker();
 	return (
-		<TouchableOpacity>
-			<Card style={stylePhoto.add}>
+		<TouchableOpacity
+			style={stylePhoto.add}
+			disabled={isLoading}
+			onPress={() => pickImage([1, 1])}>
+			{image ? (
+				<Image style={stylePhoto.image} source={{ uri: image }} />
+			) : (
 				<Ionicons
 					name='add-circle'
 					size={48}
 					color={COLORS.white}
 				/>
-			</Card>
+			)}
 		</TouchableOpacity>
 	);
 };
@@ -28,5 +33,11 @@ const stylePhoto = StyleSheet.create({
 		height: 200,
 		justifyContent: 'center',
 		alignItems: 'center',
+		borderRadius: 10,
 	},
+	image: {
+		width: '100%',
+		height: '100%',
+		borderRadius: 10
+	}
 });
